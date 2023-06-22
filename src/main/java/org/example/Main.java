d89d83a7ac6d7d4603a50ef50ab62853a1826820
Elvira <elviraw@mail.ru>
2023-06-22 13:55:13 +0300

    lesson2, added optimization of the victory method

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,223 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final char DOT_HUMAN = 'X';
+
+    private static final char DOT_AI = 'O';
+
+    private static final char DOT_Empty = '.';
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private static char[][] field; // Двумерный массив хранит текущее состояние игрового поля
+
+    private static final Random random = new Random();
+
+    private static final int WINNER= 4;
+
+    private static int fieldSizeX;
+    private static int fieldSizeY;
+
+
+    public static void main(String[] args) {
+    while (true) {
+        initialize();
+        printField();
+        while (true) {
+            humanTurn();
+            printField();
+            if (gameCheck(DOT_HUMAN, "Вы победили!"))
+                break;
+            airTurn();
+            printField();
+            if (gameCheck(DOT_AI, "Компьютер победил!"))
+                break;
+
+        }
+        System.out.println("Желаете сыграть ещё раз? (Y - да)");
+        if (SCANNER.next().equalsIgnoreCase("Y"))
+            break;
+    }
+    }
+
+    /**
+     * Инициализация игрового поля
+     */
+    private static void initialize(){
+        // Установим размерность игрового поля
+        fieldSizeX=5;
+        fieldSizeY=5;
+
+        field = new char[fieldSizeX][fieldSizeY];
+        // Пройдем по всем элементам массива
+        for(int x=0;x<fieldSizeX;x++){
+            for(int y=0;y<fieldSizeY;y++){
+                // Проинициализируем все элементы массива DOT_Empty
+                field[x][y] = DOT_Empty;
+            }
+        }
+    }
+
+    /**
+     *  Отрисовка игрового поля
+     */
+    private static void printField(){
+        System.out.print("+");
+        for(int i = 0;i < fieldSizeY* 2 + 1; i++){
+            System.out.print(i % 2 == 0 ? "-" : i / 2 + 1);
+        }
+        System.out.println();
+
+        for(int i = 0; i<fieldSizeX;i++){
+            System.out.print(i+1+"|");
+            for(int j = 0; j < fieldSizeY;j++){
+                System.out.print(field[i][j] + "|");
+            }
+            System.out.println();
+        }
+
+
+        for(int i = 0;i < fieldSizeX * 2 + 2; i++){
+            System.out.print("-");
+        }
+        System.out.println();
+
+    }
+
+    /**
+     *  Обработка хода игрока (человек)
+     */
+    private static void humanTurn(){
+        int x,y;
+        do {
+
+            System.out.print("Введите координаты хода X и Y (от 1 до 3) через пробел >>");
+            x = SCANNER.nextInt()-1;
+            y = SCANNER.nextInt()-1;
+        }
+        while (!isCellValid(x,y) || !isCellEmpty(x,y));
+        field[x][y] = DOT_HUMAN;
+
+    }
+
+    /**
+     * Ход компьютера
+     */
+    private static void airTurn(){
+        int x,y;
+        do{
+            x = random.nextInt(fieldSizeX);
+            y = random.nextInt(fieldSizeY);
+        }
+        while (!isCellEmpty(x,y));
+        field[x][y] = DOT_AI;
+}
+    /**
+     * Проверка, ячейка является пустой
+     * @param x
+     * @param y
+     * @return
+     */
+    static boolean isCellEmpty(int x, int y){
+        return field[x][y] == DOT_Empty;
+    }
+
+    /**
+     * Проверка корректности ввода
+     * (координаты хода не должны превышать размерность массива, игрового поля)
+     * @param x
+     * @param y
+     * @return
+     */
+    static boolean isCellValid(int x, int y){
+        return x>=0 && x < fieldSizeX && y>=0 && y < fieldSizeY;
+    }
+
+    /**
+     * Проверка победы
+     * Переработанный метод, через циклы
+     * @param c
+     * @return
+     */
+    static boolean checkWin(char c){
+        int count=0;
+        // Проверка по трем горизонталям
+//        if(field[0][0] == c && field[0][1]== c && field[0][2] == c) return true;
+//        if(field[1][0] == c && field[1][1]== c && field[1][2] == c) return true;
+//        if(field[2][0] == c && field[2][1]== c && field[2][2] == c) return true;
+        for(int i = 0;i < fieldSizeX;i++){
+            count=0;
+            for(int j = 0; j < fieldSizeY;j++){
+                if(field[i][j]==c) count++;
+                if(count==WINNER) return true;
+            }
+
+        }
+        // Проверка по диагоналям
+//        if(field[0][0] == c && field[1][1]== c && field[2][2] == c) return true;
+//        if(field[0][2] == c && field[1][1]== c && field[2][0] == c) return true;
+        count=0;
+        for(int i = 0;i < fieldSizeX;i++){
+            if(field[i][i]==c)count++;
+            if(count==WINNER) return true;
+        }
+        count = 0;
+        for(int i = 0;i < fieldSizeY;i++){
+            if(field[i][fieldSizeY-1-i]==c)count++;
+            if(count==WINNER) return true;
+        }
+
+
+        // Проверка по трем вертикалям
+//        if(field[0][0] == c && field[1][0]== c && field[2][0] == c) return true;
+//        if(field[0][1] == c && field[1][1]== c && field[2][1] == c) return true;
+//        if(field[0][2] == c && field[1][2]== c && field[2][2] == c) return true;
+        for(int i = 0;i < fieldSizeX;i++){
+            count=0;
+            for(int j = 0; j < fieldSizeY;j++){
+                if(field[j][i]==c) count++;
+                if(count==WINNER) return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    /**
+     * Проверка на ничью
+     * @return
+     */
+    static boolean checkDraw(){
+        for(int x = 0; x < fieldSizeX; x++){
+            for(int y=0; y< fieldSizeY;y++)
+            {
+                if(isCellEmpty(x,y)) return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Метод проверки состояния игры
+     * @param c
+     * @param str
+     * @return
+     */
+    static boolean gameCheck(char c, String str){
+        if(checkWin(c)){
+            System.out.println(str);
+            return true;
+        }
+        if(checkDraw()){
+            System.out.println("Ничья!");
+        }
+        return false; // Игра продолжается
+    }
+
+
+}
